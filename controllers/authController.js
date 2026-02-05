0eccc12c353e2852567a23289203646e7e2ffa25
NorbertLoh <lohnorbert@gmail.com>
2026-02-06 00:49:17 +0800

    Fix bugs for loginController

--- controllers/authController.js
+++ controllers/authController.js
@@ -115,42 +115,55 @@ export const registerController = async (req, res) => {
 //POST LOGIN
 export const loginController = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    let { email, password } = req.body;
+
+    // trim inputs
+    email = email?.trim();
+    password = password?.trim();
+
+    const invalidError = "Invalid Email or Password";
     //validation
     if (!email || !password) {
-      return res.status(404).send({
+      return res.status(400).send({
         success: false,
-        message: "Invalid email or password",
+        message: invalidError,
       });
     }
     //check user
     const user = await userModel.findOne({ email });
     if (!user) {
-      return res.status(404).send({
+      return res.status(400).send({
         success: false,
-        message: "Email is not registerd",
+        message: invalidError,
       });
     }
     const match = await comparePassword(password, user.password);
     if (!match) {
-      return res.status(200).send({
+      return res.status(400).send({
         success: false,
-        message: "Invalid Password",
+        message: invalidError,
       });
     }
+
+    const emailRegex = /^((?:[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]|(?<=^|\.)"|"(?=$|\.|@)|(?<=".*)[ .](?=.*")|(?<!\.)\.){1,64})(@)((?:[A-Za-z0-9.\-])*(?:[A-Za-z0-9])\.(?:[A-Za-z0-9]){2,})$/gm;
+    if (!emailRegex.test(email)) {
+      return res.status(400).send({ success: false, message: invalidError });
+    }
+
     //token
     const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
     res.status(200).send({
       success: true,
-      message: "login successfully",
+      message: "Login Successful",
       user: {
         _id: user._id,
         name: user.name,
         email: user.email,
         phone: user.phone,
         address: user.address,
+        DOB: user.DOB,
         role: user.role,
       },
       token,
@@ -159,7 +172,7 @@ export const loginController = async (req, res) => {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error in login",
+      message: "Error in Login",
       error,
     });
   }
