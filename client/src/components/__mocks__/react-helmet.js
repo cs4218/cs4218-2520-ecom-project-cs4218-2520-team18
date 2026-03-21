319c288eedf7228825ce051eb51d86199bce0f82
SherwynNg <sherwyn2000@hotmail.com>
2026-03-21 14:29:41 +0800

    resolve CICD test fails

--- client/src/components/__mocks__/react-helmet.js
+++ client/src/components/__mocks__/react-helmet.js
@@ -1,26 +1,18 @@
 import React, { useEffect } from 'react';
 
 export function Helmet({ children }) {
-  useEffect(() => {
-    React.Children.forEach(children, (child) => {
-      if (React.isValidElement(child)) {
-        if (child.type === 'title') {
-          document.title = child.props.children;
-        }
-        if (child.type === 'meta') {
-          return React.cloneElement(child, {
-            ...child.props,
-            'data-testid': 'helmet-meta',
-          });
-        }
-      }
-    });
-    return () => {
-      const metas = document.head.querySelectorAll(
-        'meta[data-testid="helmet-meta"]',
-      );
-      metas.forEach((meta) => meta.remove());
-    };
-  }, [children]);
-  return <div data-testid="helmet-mock">{children}</div>;
+  const enhancedChildren = React.Children.map(children, (child) => {
+    if (React.isValidElement(child) && child.type === 'meta') {
+      return React.cloneElement(child, {
+        ...child.props,
+        'data-testid': 'helmet-meta',
+      });
+    }
+    return child;
+  });
+  return (
+    <div data-testid="helmet-mock">
+      {enhancedChildren}
+    </div>
+  );
 }
